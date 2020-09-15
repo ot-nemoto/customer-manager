e3784c6a9fec4c084ac05a8cba37332d40098d27
ot-nemoto <nemoto@opentone.co.jp>
2020-09-15 12:45:53 +0000

    add Dockerfile(s)
    - Dockerfileおよびdocker-compose.ymlを追加
    - ALLOWED_HOSTSに `'*'` を設定

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ja6iwvdw!q5rtpopd1=i)&llw@4_^(3pbya1n^hxbrqz2knu7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
