b5be2a2cf23f626b08537789c4c165e1da10d336
ot-nemoto <nemoto@opnetone.co.jp>
2020-09-16 00:42:07 +0000

    ライブラリを追加した
    - djangorestframework
    - drf_yasg

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="customer-manager",
+        default_version='v1',
+        description="customer-manager",
+        terms_of_service="https://github.com/nemodija/customer-manager",
+        contact=openapi.Contact(email="nemoto@opentone.co.jp"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
