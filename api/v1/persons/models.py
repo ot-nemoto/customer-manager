010f0eafd5f315d1392ecdfdb97519b9f481b03b
ot-nemoto <nemoto@opnetone.co.jp>
2020-09-16 14:42:16 +0000

    データベース名をデフォルトではなくそれぞれ指定した

--- api/v1/persons/models.py
+++ api/v1/persons/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        db_table = 'persons'
+
     def __str__(self):
         return self.name
--- api/v1/sections/models.py
+++ api/v1/sections/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Section(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        db_table = 'sections'
+
     def __str__(self):
         return self.name
